7dfa549d58e6dbd8d91ba447e40d9a88e0429c3b
Maykon <maykon.camargo@al.infnet.edu.br>
2026-05-09 21:07:44 -0300

    feat(): implementa a classe produto

---
+++ produto.py
@@ -0,0 +1,8 @@
+class Produto:
+    def __init__(self, id, nome, quantidade, estoque, preco):
+        self.id = id
+        self.nome = nome
+        self.quantidade = quantidade
+        self.estoque = estoque
+        self.preco = preco
+
