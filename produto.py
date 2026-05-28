54f2b3da1dfeef22418dfcd3119bf79a103e609a
Maykon <maykon.camargo@al.infnet.edu.br>
2026-05-27 21:01:05 -0300

    alteraçao nome produto pra models

--- produto.py
+++
@@ -1,8 +0,0 @@
-class Produto:
-    def __init__(self, id, nome, quantidade, estoque, preco):
-        self.id = id
-        self.nome = nome
-        self.quantidade = quantidade
-        self.estoque = estoque
-        self.preco = preco
-
